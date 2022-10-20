9695cfe5ca89cc5fb62b4e61e2a7eaa35a5c7e31
Vincent <vincentmolinare@gmail.com>
2022-10-20 10:59:30 +0300

    Return Tie result for rock

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+
+//Return random array item
 function getComputerChoice(rps)
 {
 return rps[Math.floor(Math.random()*rps.length)]; 
 }
 let rps = ['Rock', 'Paper', 'Scissor']
-console.log(getComputerChoice(rps));
+
+//console.log(getComputerChoice(rps));
+
+function playRound(playerSelection, computerSelection)
+{
+if (playerSelection === 'Rock')
+{
+    return 'Tie game!';
+}
+
+
+
+
+
+
+
+}
+    let playerSelection = 'Rock';
+    let computerSelection = getComputerChoice(rps);
+    console.log(playRound(playerSelection, computerSelection));
